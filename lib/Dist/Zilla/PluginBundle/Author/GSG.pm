c9dc44a8af5546fd17a108b46a37a48729f5c8a9
Andrew Fresh <andrew.fresh@grantstreet.com>
2019-03-05 17:15:38 -0500

    Initial commit

---
+++ lib/Dist/Zilla/PluginBundle/Author/GSG.pm
@@ -0,0 +1,19 @@
+package Dist::Zilla::PluginBundle::Author::GSG;
+
+# ABSTRACT: Grant Street Group CPAN dists
+# VERSION
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+Your C<dist.ini> can be as short as this:
+
+    name = Foo-Bar-GSG
+    [@Author::GSG]
+
+Which is equivalent to all of this:
+
+=cut
