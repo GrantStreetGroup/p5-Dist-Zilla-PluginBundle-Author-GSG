aeed114e2db489f2f2ef3fe2e4b25266899563e7
Andrew Fresh <andrew@afresh1.com>
2021-02-09 17:22:54 -0900

    Correctly include Makefile.inc in example

--- lib/Dist/Zilla/PluginBundle/Author/GSG.pm
+++ lib/Dist/Zilla/PluginBundle/Author/GSG.pm
@@ -473,7 +473,7 @@ might look like this:
         carton exec perl -Ilib -MFile::ShareDir=dist_dir -e \
             'print eval { dist_dir("Dist-Zilla-PluginBundle-Author-GSG") } || "share"' )
 
-    include $(SHARE_DIR)/Makefile
+    include $(SHARE_DIR)/Makefile.inc
 
     # Copy the SHARE_DIR Makefile over this one:
     # Making it .PHONY will force it to copy even if this one is newer.
